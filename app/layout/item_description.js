9dabbac3545aa816fb39810bea095d6e61430686
Yuri Ramos <yuri.pramos@gmail.com>
2017-03-24 12:14:38 -0300

    sharpening styles and testing responsive

--- app/layout/item_description.js
+++ app/layout/item_description.js
@@ -1,24 +1,31 @@
 import React from "react";
-
+import FontAwesome from 'react-fontawesome';
 
 const ItemDescription = ({item}) => {
 	if(!item){
 		return <div>Loading...</div>;
 	}
 	const ItemId = item.id;	
-	const userImage = item.user.avatar_url;
-	const userName = item.user.name;
-	const tags = item.tags.map((tag) => { return <span className="tags">{tag} &nbsp;</span> });
+	const UserImage = item.user.avatar_url;
+	const UserName = item.user.name;
+	const ItemDesc = item.description;
+	const Tags = item.tags.map((tag) => { return <span className="tags">
+		<FontAwesome
+		className='font-awesome'
+        name='tag'
+	    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> {tag} &nbsp;</span> });
+
 	return (
 		<div className="container">
 			<div className="selected-detail col-md-12 col-md-12 col-sm-12 col-xs-12 text-center">
 				<div className="main-description col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2 col-xs-12">
-					<img className="media-object" src={userImage}/> 
-					{userName} <br/>
-					{tags}
+					<img className="media-avatar" src={UserImage}/> 
+					{UserName} <br/>
+					{Tags}
 				</div>
 				<div className="details col-lg-offset-2 col-lg-4 col-md-offset-2 col-md-4 col-lg-offset-2 col-sm-4 col-xs-12 text-center">
 					<div><img className="media-object" src={item.images.normal}/></div>
+					<p dangerouslySetInnerHTML={{__html: ItemDesc}} />
 				</div>
 			</div>
 		</div>
--- app/layout/item_list_detail.js
+++ app/layout/item_list_detail.js
@@ -1,19 +1,45 @@
 import React from "react";
+import FontAwesome from 'react-fontawesome';
 import ItemDescription from "./item_description.js";
 
 
 const ItemListDetail = ({item, onItemSelect}) => {
-	const imageUrl = item.images.teaser;
+	const ImageUrl = item.images.teaser;
+	const Attachments = item.attachments_count;
+	const ViewsCount = item.views_count;
+	const CommentsCount = item.comments_count;
+	const LikesCount = item.likes_count;
 
 	return( 
-		<li onClick ={() => onItemSelect(item) } className="col-lg-4 col-md-4 col-sm-6 col-xs-12 list-group-item">
-			<div className="video-list media">
+		<li onClick ={() => onItemSelect(item) } className="col-lg-3 col-md-4 col-sm-4 col-xs-12 list-group-item">
+			<div className="shot-list media">
 				<div className="media-left">
-					<img className="media-object" src={imageUrl}/>
+					<img className="media-object" src={ImageUrl}/>
 				</div>
 
 				<div className="media-body">
-					<div className="media-heading">Views: {item.views_count} Comments:{item.comments_count} Likes:{item.likes_count}</div>
+					<div className="media-heading">      
+						<FontAwesome
+				        className='font-awesome'
+				        name='eye'
+				        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> 
+				        {ViewsCount} 
+				        <FontAwesome
+				        className='font-awesome'
+				        name='comments-o'
+				        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> 
+				        {CommentsCount} 
+				        <FontAwesome
+				        className='font-awesome'
+				        name='heart'
+				        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> 
+				        {LikesCount} 
+				        <FontAwesome
+				        className='font-awesome'
+				        name='paperclip'
+				        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> 
+				        {Attachments}
+				    </div>
 				</div>
 			</div>
 		</li>
