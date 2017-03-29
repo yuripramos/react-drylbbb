f41dae2e9e0f6880de1c5f0179b60f54132639dc
Yuri Ramos <yuri.pramos@gmail.com>
2017-03-29 09:57:20 -0300

    creating helpers for authentication

--- app/layout/app.js
+++ app/layout/app.js
@@ -5,13 +5,7 @@ import GitHubForkRibbon from 'react-github-fork-ribbon';
 import Footer from "./footer.js";
 import ItemList from "./item_list.js";
 import ItemDescription from "./item_description.js";
-
-const AUTH_TOKEN = "20247ff2713d41fea7dd0ba106834d2ebf11f4b9f663c0eb405ae69976fcc4d3";
-
-axios.defaults.baseURL = 'https://api.dribbble.com/v1/';
-axios.defaults.headers.common['Authorization'] =`Bearer ${AUTH_TOKEN}`; //New in ES6
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+import auth from "../helpers/auth.js";
 
 
 class Main extends React.Component {
